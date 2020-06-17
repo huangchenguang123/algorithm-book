d65358b57e7b99ae783a0caf0cd9610446b9fd54
huangchenguang <923622908@qq.com>
2020-06-17 23:45:47 +0800

    [commit]: 2020-06-17
    push 2 answer

---
+++ scala/leetcode/q28/Solution.scala
@@ -0,0 +1,36 @@
+package leetcode.q28
+
+class Solution {}
+
+object Solution {
+
+  def main(args: Array[String]): Unit = {
+    val haystack = "hello"
+    val needle = "ll"
+    val result = strStr(haystack, needle)
+    print(result)
+  }
+
+  /**
+   * @author : chenguang
+   * @date 2020/6/17
+   * @desc : subString实现，还有其他更加优秀的算法
+   */
+  /**
+   * @date 2020/6/17
+   * @result: true
+   * @time: 80%
+   * @memory usage: 100%
+   */
+  def strStr(haystack: String, needle: String): Int = {
+    val length = needle.length
+    if (length == 0) return 0
+    for (i <- 0 to (haystack.length - length)) {
+      if (haystack(i) == needle(0) && haystack.substring(i, i + length).equals(needle)) {
+          return i
+      }
+    }
+    -1
+  }
+
+}
