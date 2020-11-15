89c5a8fec2868e891da65cfa2738617fb0931edc
huangchenguang <923622908@qq.com>
2020-11-16 00:27:40 +0800

    2020-11-16 fix 1 answer

--- java/leetcode/q1/SolutionJ.java
+++ java/leetcode/q1/SolutionJ.java
@@ -9,7 +9,7 @@ public class SolutionJ {
     public static void main(String[] args) {
         int[] nums = new int[]{2, 7, 11, 15};
         int target = 9;
-        Solution solution = new Solution();
+        SolutionJ solution = new SolutionJ();
         System.out.println(Arrays.toString(solution.twoSum(nums, target)));
     }
 
--- java/leetcode/q26/SolutionJ.java
+++ java/leetcode/q26/SolutionJ.java
@@ -4,7 +4,7 @@ public class SolutionJ {
 
     public static void main(String[] args) {
         int[] nums = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        Solution solution = new Solution();
+        SolutionJ solution = new SolutionJ();
         System.out.println(solution.removeDuplicates(nums));
     }
 
@@ -17,12 +17,12 @@ public class SolutionJ {
         }
         int index = 0;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i]) {
+            if (nums[i] != nums[index]) {
                 index++;
                 nums[index] = nums[i];
             }
         }
-        return index;
+        return index + 1;
     }
 
 }
