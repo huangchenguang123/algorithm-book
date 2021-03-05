6ceefd1141124add8108a6db3aa0fc24f1e3fa09
huangchenguang <923622908@qq.com>
2021-03-05 23:29:11 +0800

    2021-03-05 push 2 answer

---
+++ java/leetcode/offer10_1/SolutionJ.java
@@ -0,0 +1,32 @@
+package leetcode.offer10_1;
+
+/**
+ * @date 2021/3/5
+ * @author chenguang
+ * @desc 剑指 Offer 10- I. 斐波那契数列
+ *
+ * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）
+ */
+public class SolutionJ {
+
+    public static void main(String[] args) {
+        System.out.println(new SolutionJ().fib(5));
+    }
+
+    public int fib(int n) {
+        int fn = 0, fn1 = 1, sum = 0;
+        if (n == 0) {
+            return fn;
+        }
+        if (n == 1) {
+            return fn1;
+        }
+        for (int i = 2; i <= n; i++) {
+            sum = (fn + fn1) % 1000000007;
+            fn = fn1;
+            fn1 = sum;
+        }
+        return sum;
+    }
+
+}
--- java/leetcode/offer11/SolutionJ.java
+++ java/leetcode/offer11/SolutionJ.java
@@ -3,30 +3,28 @@ package leetcode.offer11;
 /**
  * @date 2021/3/5
  * @author chenguang
- * @desc 剑指 Offer 10- I. 斐波那契数列
- *
- * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）
+ * @desc 
  */
 public class SolutionJ {
 
     public static void main(String[] args) {
-        System.out.println(new SolutionJ().fib(5));
+
     }
 
-    public int fib(int n) {
-        int fn = 0, fn1 = 1, sum = 0;
-        if (n == 0) {
-            return fn;
+    public int minArray(int[] numbers) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (numbers[mid] < numbers[high]) {
+                high = mid;
+            } else if (numbers[mid] > numbers[high]) {
+                low = mid + 1;
+            } else {
+                high -= 1;
+            }
         }
-        if (n == 1) {
-            return fn1;
-        }
-        for (int i = 2; i <= n; i++) {
-            sum = (fn + fn1) % 1000000007;
-            fn = fn1;
-            fn1 = sum;
-        }
-        return sum;
+        return numbers[low];
     }
 
 }
