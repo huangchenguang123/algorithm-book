acc46245d82fd5149fc55ba8c10f774fae534ead
huangchenguang <923622908@qq.com>
2021-03-05 23:30:05 +0800

    2021-03-05 push 2 answer

--- java/leetcode/offer11/SolutionJ.java
+++ java/leetcode/offer11/SolutionJ.java
@@ -3,7 +3,9 @@ package leetcode.offer11;
 /**
  * @date 2021/3/5
  * @author chenguang
- * @desc 
+ * @desc 剑指 Offer 11. 旋转数组的最小数字
+ *
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
  */
 public class SolutionJ {
 
