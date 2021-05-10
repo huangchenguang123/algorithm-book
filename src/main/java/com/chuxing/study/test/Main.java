d3e44ffb1bd6d1aa6af115bf152114aa8180b5ed
huangchenguang <huangchenguang@youzan.com>
2021-05-10 22:31:47 +0800

    push some case

---
+++ src/main/java/com/chuxing/study/test/Main.java
@@ -0,0 +1,14 @@
+package com.chuxing.study.test;
+
+/**
+ * @date 2021/5/10
+ * @author chenguang
+ * @desc main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println((int) (-1 & 0x7FFFFFFF));
+    }
+
+}
