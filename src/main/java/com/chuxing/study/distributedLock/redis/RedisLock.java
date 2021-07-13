7ec3bff7491e163d0eed019f261eac821dbc2e3e
huangchenguang123 <a923622908@outlook.com>
2021-07-13 22:52:36 +0800

    add redis lock

--- src/main/java/com/chuxing/study/distributedLock/redis/RedisLock.java
+++ src/main/java/com/chuxing/study/distributedLock/redis/RedisLock.java
@@ -4,6 +4,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.time.Duration;
 
 /**
  * @date 2021/7/8
@@ -22,7 +23,7 @@ public class RedisLock {
      * @desc 尝试获取分布式锁
      */
     public Boolean lock(String key) {
-        return stringRedisTemplate.opsForValue().setIfAbsent(key, key);
+        return stringRedisTemplate.opsForValue().setIfAbsent(key, key, Duration.ofSeconds(10));
     }
 
     /**
